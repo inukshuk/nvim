b1a9a4d7f479b93f7fea5fb595f6647ea54ee530
Sylvester Keil <sylvester@keil.or.at>
2023-05-20 22:01:34 +0200

    Map keys for git signs

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -1,3 +1,20 @@
 vim.cmd.packadd 'gitsigns'
 
-require('gitsigns').setup()
+local gs = require 'gitsigns'
+
+gs.setup {
+  on_attach = function(bufnr)
+    vim.keymap.set('n', '[c', gs.prev_hunk, {
+      buffer = bufnr,
+      desc = 'Go to Previous Hunk'
+    })
+    vim.keymap.set('n', ']c', gs.next_hunk, {
+      buffer = bufnr,
+      desc = 'Go to Next Hunk'
+    })
+    vim.keymap.set('n', '<leader>ph', gs.preview_hunk, {
+      buffer = bufnr,
+      desc = '[P]review [H]unk'
+    })
+  end
+}
