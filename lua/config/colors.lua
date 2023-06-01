afebed65157ea52a1f27ee5df57c279308e6ca5f
Sylvester Keil <sylvester@keil.or.at>
2023-06-02 00:48:02 +0200

    Colors

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -1,9 +1,17 @@
 vim.cmd.packadd 'lush'
 vim.cmd.packadd 'zenbones'
+vim.cmd.packadd 'monochrome'
+vim.cmd.packadd 'gruvbox'
 
 vim.o.termguicolors = true
 
-vim.g.zenwritten = {
+require('gruvbox').setup {
+  contrast = 'soft',
+  dim_inactive = true,
+  transparent_mode = true
+}
+
+local zenbones_config = {
   lightness = 'dim',
   darkness = 'warm',
   darken_noncurrent_window = true,
@@ -13,5 +21,18 @@ vim.g.zenwritten = {
   darken_comments = 60,
   lighten_comments = 40,
 }
+
+vim.g.duckbones = zenbones_config
+vim.g.forestbones = zenbones_config
+vim.g.kanagawabones = zenbones_config
+vim.g.neobones = zenbones_config
+vim.g.nordbones = zenbones_config
+vim.g.rosebones = zenbones_config
+vim.g.seoulbones = zenbones_config
+vim.g.tokyobones = zenbones_config
+vim.g.vimbones = zenbones_config
+vim.g.zenbones = zenbones_config
+vim.g.zenburned = zenbones_config
+vim.g.zenwritten = zenbones_config
 
 vim.cmd.colorscheme 'zenwritten'
