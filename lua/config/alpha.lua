582173f833b364f38e602b7988bf1864507df2e1
Sylvester Keil <sylvester@keil.or.at>
2023-06-02 10:55:05 +0200

    Adjust alpha config

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -1,6 +1,12 @@
 vim.cmd.packadd 'dev-icons'
 vim.cmd.packadd 'alpha'
 
-require('alpha').setup(
-  require('alpha.themes.startify').config
-)
+local config = require('alpha.themes.startify').config
+
+config.layout[1].val = 3
+config.layout[2].val = {
+  ' N E O V I M'
+}
+config.layout[3].val = 2
+
+require('alpha').setup(config)
